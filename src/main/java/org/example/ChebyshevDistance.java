59977815163599e17797025f93378d025c33148f
AgJetta <247809@edu.p.lodz.pl>
2025-04-13 16:27:18 +0200

    Distance Metrics for numerical Features

---
+++ src/main/java/org/example/ChebyshevDistance.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.List;
+
+public class ChebyshevDistance implements DistanceMetric {
+
+    @Override
+    public double calculate(Document doc1, Document doc2) {
+        List<Integer> vec1 = doc1.getFeatures().getNumericFeatures();
+        List<Integer> vec2 = doc2.getFeatures().getNumericFeatures();
+
+        double maxDiff = 0;
+        for (int i = 0; i < vec1.size(); i++) {
+            double diff = Math.abs(vec1.get(i) - vec2.get(i));
+            if (diff > maxDiff) {
+                maxDiff = diff;
+            }
+        }
+        return maxDiff;
+    }
+}
